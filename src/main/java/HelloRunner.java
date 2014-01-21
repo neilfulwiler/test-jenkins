ed741fdf7833e4f59613a7fb3610ad71ddaa9222
Neil Fulwiler <neil@optimizely.com>
2014-01-20 22:54:55 -0800

    greeting someone else

--- src/main/java/HelloRunner.java
+++ src/main/java/HelloRunner.java
@@ -2,7 +2,7 @@ package main;
 
 public class HelloRunner {
   public static void main(String[] args) {
-    Hello hello = new Hello("World");
+    Hello hello = new Hello("You");
     hello.sayHi();
     return;
   }
